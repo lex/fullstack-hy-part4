f11a9ce296c5c8215ad5eb6c592ff3a7a2025261
lex <lex@users.noreply.github.com>
2018-02-05 16:12:52 +0200

    refactor default like behavior

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -41,10 +41,6 @@ blogsRouter.post("/", async (request, response) => {
 
     const blog = new Blog(request.body);
 
-    if (blog.likes === undefined) {
-      blog.likes = 0;
-    }
-
     const result = await blog.save();
     response.status(201).json(result);
   } catch (exception) {
