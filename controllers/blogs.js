83f12ac9bcca819448a7d88fa9964c6715c709ec
lex <lex@users.noreply.github.com>
2018-02-05 20:45:54 +0200

    4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = request => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   try {
@@ -31,7 +40,13 @@ blogsRouter.get("/:id", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   try {
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
     const body = request.body;
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
 
     if (body.title === undefined) {
       return response.status(400).json({ error: "title missing" });
@@ -45,7 +60,7 @@ blogsRouter.post("/", async (request, response) => {
       return response.status(400).json({ error: "url missing" });
     }
 
-    const user = await User.findOne({});
+    const user = await User.findById(decodedToken.id);
 
     const blog = new Blog({
       author: body.author,
